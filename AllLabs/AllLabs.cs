3e996503f798fc91f677d0d00eebf9c0b1e40327
shoot <mr.fostik@mail.ru>
2021-09-28 17:36:10 +0700

    Теперь создаёт новое окно при нажатии на Лабораторная Работа 1 всегда

--- AllLabs/AllLabs.cs
+++ AllLabs/AllLabs.cs
@@ -6,19 +6,16 @@ namespace AllLabs
 {
     public partial class AllLabs : Form
     {
-        private Lab1Form _lab1;
         public AllLabs()
         {
             InitializeComponent();
-            _lab1 = null;
         }
 
         private void Lab1Button_Click(object sender, EventArgs e)
         {
-            if(_lab1==null)
-                _lab1 = new Lab1Form();
+            var lab1 = new Lab1Form();
             
-            _lab1.Show();
+            lab1.Show();
         }
 
         private void button1_Click(object sender, EventArgs e)
